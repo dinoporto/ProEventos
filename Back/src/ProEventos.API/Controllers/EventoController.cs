624c1beb42b48d9dfa1a29116b5fbdf4bcb002be
Dino Porto <dinopc@gmail.com>
2022-01-01 16:14:25 -0300

    Sessão 3: EF Core 5 - Introdução

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,67 +13,49 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-       
-      IEnumerable<Evento>  _eventos = new Evento[]{ new Evento(){EventoId=1,
-                                              Local="Salvador",
-                                              DataEvento="2022-01-01",
-                                              Tema="Ano Novo",
-                                              qtdPessoas=1000,
-                                              Lote="123",
-                                              ImagemURL=""
-                                            },
-                                      new Evento(){EventoId=2,
-                                                Local="Lauro de Freitas",
-                                                DataEvento="2022-01-01",
-                                                Tema="Ano Novo dos velhos",
-                                                qtdPessoas=500,
-                                                Lote="1",
-                                                ImagemURL=""
-                                            }
-                                            
-                                };
 
-        public EventoController()
+        private readonly DataContext contexto;
+
+        public EventoController(DataContext contexto)
         {
-           
+            this.contexto = contexto;
+
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            
-            return _eventos;
-
+            return contexto.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-            
-            return _eventos.Where(e => e.EventoId == id);
+
+            return contexto.Eventos.FirstOrDefault(e => e.EventoId == id);
 
         }
 
         [HttpPost]
         public string FakeInsert()
         {
-           
+
             return "eu inseri o ódio.";
 
         }
 
-         [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public string FakePut(int id)
         {
-           
+
             return $"eu inseri o ódio com id = {id}";
 
         }
 
-          [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
         public string FakeDelete(int id)
         {
-           
+
             return $"eu deletei o ódio com id = {id}";
 
         }
